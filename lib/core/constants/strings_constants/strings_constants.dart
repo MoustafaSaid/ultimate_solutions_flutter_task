b63b813487ac17df8e8a0f220eceabeb750b370c
moustafa said salha <100960738+MoustafaSaid@users.noreply.github.com>
2025-04-24 17:02:08 +0200

    Initialize Flutter project _ Add core packages to pubspec.yaml _ Setup assets directory and resource organization _ Create constants for colors, strings, and dimensions

---
+++ lib/core/constants/strings_constants/strings_constants.dart
@@ -0,0 +1,23 @@
+class StringsConstants {
+  static String welcomeBack="welcomeBack""Welcome Back!";
+  static String logBack="logBack""Log back into your account";
+  static String userId="userId"" User ID";
+  static String password="password""Password ";
+  static String showMore="showMore""Show More ";
+  static String logIn="logIn""Log in ";
+  static String chooseLanguage="chooseLanguage""Choose Language ";
+  static String apply="apply"" Apply";
+  static String arabic="arabic"" Arabic";
+  static String english="english""English ";
+  static String others="others"" Others";
+  static String noOrdersTitle="noOrdersTitle""No orders yet ";
+  static String noOrdersSubTitle="noOrdersSubTitle""You don't have any orders in your history. ";
+  static String status="status"" Status";
+  static String totalPrice="totalPrice"" Total price";
+  static String date="date""Date ";
+  static String orderDetails="orderDetails""Order Details ";
+  static String delivering="delivering""Delivering ";
+  static String returned="returned"" Returned";
+  static String delivered="delivered"" Delivered";
+  static String newItems="newItems"" New";
+}
---
+++ lib/core/theme/font_manager/font_styles.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ultimate_solution_flutter_task/core/theme/colors_manager/colors_manager.dart';
+import 'package:ultimate_solution_flutter_task/core/theme/font_manager/font_constants.dart';
+
+class TextStyles {
+  static TextStyle font29primaryDarkSemiBold = TextStyle(
+      fontSize: FontSize.s30.sp,
+      color: ColorsConstants.primaryDark,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.semiBold);
+  static TextStyle font12primaryDarkBold = TextStyle(
+      fontSize: FontSize.s12.sp,
+      color: ColorsConstants.primaryDark,
+      fontFamily: FontConstant.fontFamily2,
+      fontWeight: FontWeightManager.bold);
+  static TextStyle font12primaryDarkSemiBold = TextStyle(
+      fontSize: FontSize.s12.sp,
+      color: ColorsConstants.primaryDark,
+      fontFamily: FontConstant.fontFamily2,
+      fontWeight: FontWeightManager.semiBold);
+  static TextStyle font10blackRegular = TextStyle(
+      fontSize: FontSize.s10.sp,
+      color: ColorsConstants.black,
+      fontFamily: FontConstant.fontFamily2,
+      fontWeight: FontWeightManager.regular);
+  static TextStyle font12blackRegular = TextStyle(
+      fontSize: FontSize.s12.sp,
+      color: ColorsConstants.black,
+      fontFamily: FontConstant.fontFamily2,
+      fontWeight: FontWeightManager.regular);
+
+  static TextStyle font16whiteSemiBold = TextStyle(
+      fontSize: FontSize.s16.sp,
+      color: ColorsConstants.primaryDark,
+      fontFamily: FontConstant.fontFamily2,
+      fontWeight: FontWeightManager.semiBold);
+  static TextStyle font14primaryDarkSemiBold = TextStyle(
+      fontSize: FontSize.s14.sp,
+      color: ColorsConstants.primaryDark,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.semiBold);
+  static TextStyle font14whiteSemiBold = TextStyle(
+      fontSize: FontSize.s14.sp,
+      color: ColorsConstants.white,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.semiBold);
+  static TextStyle font23blackSemiBold = TextStyle(
+      fontSize: FontSize.s23.sp,
+      color: ColorsConstants.white,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.semiBold);
+
+static TextStyle font12primaryDarkMedium = TextStyle(
+      fontSize: FontSize.s12.sp,
+      color: ColorsConstants.primaryDark,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.medium);
+static TextStyle font16whiteMedium = TextStyle(
+      fontSize: FontSize.s16.sp,
+      color: ColorsConstants.white,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.medium);
+static TextStyle font25whiteMedium = TextStyle(
+      fontSize: FontSize.s25.sp,
+      color: ColorsConstants.white,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.medium);
+static TextStyle font25whiteBold = TextStyle(
+      fontSize: FontSize.s25.sp,
+      color: ColorsConstants.white,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.bold);static TextStyle font8whiteRegular = TextStyle(
+      fontSize: FontSize.s8.sp,
+      color: ColorsConstants.white,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.regular);
+
+static TextStyle font14BlackRegular = TextStyle(
+      fontSize: FontSize.s14.sp,
+      color: ColorsConstants.black,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.regular);
+  static TextStyle font12darkGeyMedium = TextStyle(
+      fontSize: FontSize.s12.sp,
+      color: ColorsConstants.darkGrey,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.medium);
+  static TextStyle font10darkGeyMedium = TextStyle(
+      fontSize: FontSize.s10.sp,
+      color: ColorsConstants.darkGrey,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.medium);
+  static TextStyle font16greenSemiBold = TextStyle(
+      fontSize: FontSize.s16.sp,
+      color: ColorsConstants.success,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.semiBold);
+static TextStyle font16redSemiBold = TextStyle(
+      fontSize: FontSize.s16.sp,
+      color: ColorsConstants.error,
+      fontFamily: FontConstant.fontFamily,
+      fontWeight: FontWeightManager.semiBold);
+
+
+}
